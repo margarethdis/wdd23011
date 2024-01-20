5be8e27c7e06f7bc40669f5b6359e4be9f212a88
margarethdis <margarethdani2021@gmail.com>
2024-01-20 12:44:39 -0600

    lesson2

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtén el año actual y actualiza el primer párrafo del pie de página
+    //para obtener el año actual y actualiza el primer párrafo del pie de página
     var currentYear = new Date().getFullYear();
     var copyrightElement = document.querySelector("#footer-copyright-year");
     if (copyrightElement) {
         copyrightElement.innerHTML = '&copy; ' + currentYear;
     }
 
-    // Obtén la fecha de la última modificación y actualiza el segundo párrafo del pie de página
+    //para para obtener  la fecha de la última modificación y actualiza el segundo párrafo del pie de página
     var lastModified = new Date(document.lastModified);
     document.querySelector("#last-modified").textContent = "Last modification: " + lastModified.toLocaleString();
 
-    // Obtén el año actual y actualiza el elemento con el ID 'current-year'
+    // para obtener año actual y actualiza el elemento con el ID 'current-year'
     var currentYearElement = document.getElementById('current-year');
     if (currentYearElement) {
         currentYearElement.textContent = currentYear;
